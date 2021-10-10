2a83d88c182ac08b12f79e5dd9e75b6a8880383c
AlexandreMorvan <alex.morvan91@gmail.com>
2021-10-10 12:02:44 +0200

    ajout des donnees cards et sets

--- data/import.js
+++ data/import.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 // Je me connecte à ma BDD
 const { Client } = require('pg');
-const client = new Client("postgresql://postgres:js4life@localhost/magic");
+const client = new Client("postgresql://postgres:postgres@localhost/magic");
 
 
 const importFunction = async () => {
@@ -16,7 +16,7 @@ const importFunction = async () => {
     /// Je parcours les cartes
     for (const card of cards) {
         console.log(card.name)
-        
+
         if(card.image_uris != undefined){
             var cardImage = card.image_uris.small;
         } else {
@@ -24,19 +24,19 @@ const importFunction = async () => {
                var cardImage = elm.image_uris.small;
             }
         }
-        
-        
+
+
         console.log(card.mana_cost)
         /// Je les enregistre une à une
         const text = 'INSERT INTO cards(name, released_at, image, manacost, cmc, type_line, oracle_text, colors, set_id, set, set_name, rarity, flavor_text) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING image, name';
         const values = [card.name, card.released_at, cardImage, card.mana_cost, card.cmc, card.type_line, card.oracle_text, card.colors, card.set_id, card.set, card.set_name, card.rarity, card.flavor_text];
 
         try {
-            
+
             const res = await client.query(text, values);
             console.log(res.rows[0]);
-            
-            // le res.rows[0] correspond à un objet { id: 16, label: 'Accueil' } 
+
+            // le res.rows[0] correspond à un objet { id: 16, label: 'Accueil' }
             // la forme de cette objet est définie dans le RETURNING de l'INSERT
             cardsBDD.push(res.rows[0]);
         } catch (err) {
--- data/importSets.js
+++ data/importSets.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 // Je me connecte à ma BDD
 const { Client } = require('pg');
-const client = new Client("postgresql://postgres:js4life@localhost/magic");
+const client = new Client("postgresql://postgres:postgres@localhost/magic");
 
 
 const importFunction = async () => {
@@ -15,15 +15,15 @@ const importFunction = async () => {
     // Je les injecte en base de données
     /// Je parcours les cartes
     for (const set of sets) {
-        
+
         const text = 'INSERT INTO sets(object, id, code, tcgplayer_id, name, uri, scryfall_uri, search_uri, released_at, set_type, card_count, digital, nonfoil_only, foil_only, icon_svg_uri) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING name';
         const values = [set.object, set.id, set.code, set.tcgplayer_id, set.name, set.uri, set.scryfall_uri, set.search_uri, set.released_at, set.set_type, set.card_count, set.digital, set.nonfoil_only, set.foil_only, set.icon_svg_uri];
 
         try {
             const res = await client.query(text, values);
             console.log(res.rows[0]);
-            
-            // le res.rows[0] correspond à un objet { id: 16, label: 'Accueil' } 
+
+            // le res.rows[0] correspond à un objet { id: 16, label: 'Accueil' }
             // la forme de cette objet est définie dans le RETURNING de l'INSERT
             setsBDD.push(res.rows[0]);
         } catch (err) {
